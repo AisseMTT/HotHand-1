d88a98fa6eb6d24f6e24d03f00ccd75a99219836
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-08-17 11:41:03 +1200

    Legend

---
+++ code/display.py
@@ -0,0 +1,23 @@
+import numpy as np
+import dnest4.classic as dn4
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("data.txt")
+posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
+
+plt.plot(data, "ko", label="Data")
+plt.xlim([-0.5, len(data)-0.5])
+plt.ylim([-0.02, 1.02])
+plt.xlabel("Attempt")
+plt.ylabel("Prob(success | parameters)")
+
+for i in range(0, 100):#posterior_sample.shape[0]):
+    prob = posterior_sample[i, 3:]
+    label = None
+    if i == 0:
+        label = "Model"
+    plt.plot(prob, "g", alpha=0.1, label=label)
+
+plt.legend()
+plt.show()
+
